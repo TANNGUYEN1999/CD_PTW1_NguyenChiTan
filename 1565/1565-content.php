3c6e09eb5e0b07a9b2ea3034fb7575ae0c6765f2
Nguyễn Chí Tân <44807008+TANNGUYEN1999@users.noreply.github.com>
2020-06-16 13:59:43 +0700

    module

---
+++ 1565/1565-content.php
@@ -0,0 +1,86 @@
+<div class="type-1565">
+    <div class="row item">
+        <div class="col-md-3 item-col">
+            <div class="img-ao">
+                <span class="yellow1">
+                    <a href="#">
+                        <img class="img-fluid" src="./image/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="hinh1">
+                    </a>
+                </span>
+            </div>
+            <h3 class="tittle"> <a href=""> My favourite S/S 2020 pieces on sale</a></h3>
+            <div class="meta">
+                <a class="des" href="">Fashion advice</a>
+                <a class="des" href="">Shop the look</a> .
+                <a class="des" href=""> April 29, 2020
+                </a>
+            </div>
+            <div class="read">
+                <a class="read1" href="">
+                    READ STORY <span class="arrow"></span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3 item-col">
+            <div class="img-ao">
+                <span class="yellow1">
+                    <a href="#">
+                        <img class="img-fluid" src="./image/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="hinh1">
+                    </a>
+                </span>
+            </div>
+            <h3 class="tittle"> <a href=""> My favourite fashion trends 2020</a> </h3>
+            <div class="meta">
+                <a class="des" href="">Fashion advice</a>
+                <a class="des" href="">Trends</a> .
+                <a class="des" href=""> March 23, 2020 </a>
+            </div>
+            <div class="read">
+                <a class="read1" href="">READ STORY
+                    <span class="arrow"></span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3 item-col">
+            <div class="img-ao">
+                <span class="yellow1">
+                    <a href="#">
+                        <img class="img-fluid" src="./image/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="hinh1">
+                    </a>
+                </span>
+            </div>
+            <h3 class="tittle"> <a href="">What accessories You really need?</a> </h3>
+            <div class="meta">
+                <a class="des" href="">Accessories</a>
+                <a class="des" href="">Trends</a> .
+                <a class="des" href=""> March 22, 2020</a><br>
+            </div>
+            <div class="read">
+                <a class="read1" href="">READ STORY
+                    <span class="arrow"></span>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-3 item-col">
+            <div class="img-ao">
+                <span class="yellow1">
+                    <a href="#">
+                        <img class="img-fluid" src="./image/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="hinh1">
+                    </a>
+                </span>
+            </div>
+            <h3 class="tittle"> <a href=""> SPRING FOOTWEAR 2020</a></h3>
+            <div class="meta">
+                <a class="des" href="">SHOP THE LOOK</a>
+                <a class="des" href="">TRENDS</a> .
+                <a class="des" href=""> MARCH 22, 2020
+                </a>
+            </div>
+            <div class="read">
+                <a class="read1" href="">READ STORY
+                    <span class="arrow"></span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ 1570/1570-content.php
@@ -0,0 +1,37 @@
+ <div class="container">
+     <div class="item">
+         <div class="a2">
+             <h2 class="tittle">10 TIMELESS TRENCH COATS
+             </h2>
+             <a class="des" href="">FEBRUARY 26, 2020</a> |
+             <a class="des" href="">NO COMMENTS</a> |
+             <a class="des" href="">SHOP THE LOOK</a>
+         </div>
+
+         <div class="row a1 ">
+             <div class="col-md-6">
+                 <img class="img-fluid" src="./images/bilyana-slaveykova-YYic682HAPI-unsplash-on4j36mx158vciobczu7wlcvzlt3lxomig7x4pi6ug.png" alt="" class="img_responsive">
+             </div>
+             <div class="col-md-6">
+                 <div class="document">
+                     <span class="enlarge">E</span>
+                     <span class="document1">
+                         gestas augue ac molestie pharetra. Sed porta dui quis imperdiet consectetur. Praesent in tortor hendrerit, interdum elit blandit, volutpat nibh. Cras rutrum est et velit semper sodales. Nam nec aliquet maurisscelerisque, nunc arcu blandit diam, ac convallis lectus sem in odio. Nulla tincidunt tincidunt sem, sit amet condimentum ex mollis rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                     </span>
+                 </div>
+                 <div class="list">
+                     1. <a class="external" href=""> Robert Clergerie x Self Portrait Ankle Strap Pumps</a><br>
+                     2. <a class="external" href=""> Manolo Blahnik in Taupe</a><br>
+                     3. <a class="external" href=""> Marc Fisher Whitley Slide</a><br>
+                     4. <a class="external" href=""> Sam Edelman Brandie Flats</a><br>
+                     5. <a class="external" href=""> Jeffrey Campbell Beaton Heel</a><br>
+                     6. <a class="external" href=""> Schutz Zilu Fringe</a><br>
+                     7. <a class="external" href=""> Schutz X Revolve Lisana</a><br>
+                     8. <a class="external" href=""> Diana Jacobs Platform Sneaker</a><br>
+                     9. <a class="external" href=""> Schutz X Revolve Lisana</a><br>
+                     10.<a class="external" href=""> LV Classic Trench Coat</a><br>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>

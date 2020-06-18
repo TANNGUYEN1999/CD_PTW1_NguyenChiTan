ecebb11de1fd2c75e3732677509919aa7a6f7d08
Nguyễn Chí Tân <44807008+TANNGUYEN1999@users.noreply.github.com>
2020-06-18 22:58:58 +0700

    update-module

--- 1575/1575-content.php
+++ 1575/1575-content.php
@@ -1,51 +1,52 @@
-    <div class="container a1">
-        <h2 class="tittle">HIGHLIGHTED</h2>
-        <div class="menu">
-            <div class="row menu1">
-                <div class="col-md-3 ">
-                    <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+    <div class="type-1575">
+        <div class="container a1">
+            <h2 class="tittle">HIGHLIGHTED</h2>
+            <div class="menu">
+                <div class="row menu1">
+                    <div class="col-md-3 ">
+                        <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h2><a class="text-tittle" href=""> My favourite S/S 2020 pieces on sale </a></h2>
+                        <span class="text-date"> April 29, 2020 </span>
+                    </div>
                 </div>
-                <div class="col-md-9">
-                    <h2><a class="text-tittle" href=""> My favourite S/S 2020 pieces on sale </a></h2>
-                    <span class="text-date"> April 29, 2020 </span>
+                <div class="row menu1">
+                    <div class="col-md-3">
+                        <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h2><a class="text-tittle" href="">What accessories You really need? </a></h2>
+                        <span class="text-date"> MARCH 22, 2020 </span>
+                    </div>
                 </div>
-            </div>
-            <div class="row menu1">
-                <div class="col-md-3">
-                    <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                <div class="row menu1">
+                    <div class="col-md-3">
+                        <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h2><a class="text-tittle" href=""> 10 organic skincare products I love </a></h2>
+                        <span class="text-date"> FEBRUARY 18, 2020 </span>
+                    </div>
                 </div>
-                <div class="col-md-9">
-                    <h2><a class="text-tittle" href="">What accessories You really need? </a></h2>
-                    <span class="text-date"> MARCH 22, 2020 </span>
+                <div class="row menu1">
+                    <div class="col-md-3">
+                        <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h2><a class="text-tittle" href=""> Sweet & healthy snack ideas </a></h2>
+                        <span class="text-date"> JANUARY 20, 2020 </span>
+                    </div>
                 </div>
-            </div>
-            <div class="row menu1">
-                <div class="col-md-3">
-                    <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
-                </div>
-                <div class="col-md-9">
-                    <h2><a class="text-tittle" href=""> 10 organic skincare products I love </a></h2>
-                    <span class="text-date"> FEBRUARY 18, 2020 </span>
-                </div>
-            </div>
-            <div class="row menu1">
-                <div class="col-md-3">
-                    <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
-                </div>
-                <div class="col-md-9">
-                    <h2><a class="text-tittle" href=""> Sweet & healthy snack ideas </a></h2>
-                    <span class="text-date"> JANUARY 20, 2020 </span>
-                </div>
-            </div>
-            <div class="row menu1">
-                <div class="col-md-3">
-                    <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
-                </div>
-                <div class="col-md-9">
-                    <h2><a class="text-tittle" href=""> Aecenas vitae leo pretium sapien commodo </a></h2>
-                    <span class="text-date"> JANUARY 11, 2020 </span>
+                <div class="row menu1">
+                    <div class="col-md-3">
+                        <img class="img-fluid" src="./images/yellow-and-black-leather-cross-body-bag-2002717-650x433.png" alt="">
+                    </div>
+                    <div class="col-md-9">
+                        <h2><a class="text-tittle" href=""> Aecenas vitae leo pretium sapien commodo </a></h2>
+                        <span class="text-date"> JANUARY 11, 2020 </span>
+                    </div>
                 </div>
             </div>
         </div>
-
     </div>
--- 1580/1580-content.php
+++ 1580/1580-content.php
@@ -1,17 +1,18 @@
-<div class="container a1">
-    <div class="tittle1">
-        <h4 class="text-tittle1">
-            Lindy Harper
-        </h4>
+<div class="type-1580">
+    <div class="container a1">
+        <div class="tittle1">
+            <h4 class="text-tittle1">
+                Lindy Harper
+            </h4>
+        </div>
+        <div class="tittle2">
+            <h5 class="text-tittle2">
+                Stylist & fashion editor
+            </h5>
+        </div>
+        <img class="img-fluid image-show" src="./images/wenyang-CY_c0cR01AQ-unsplash-768x1024.png" alt="" class="img-responsive">
+        <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae voluptatem sed dignissimos sit cumque iusto! Corporis, labore? Id sequi magnam vel cum error adipisci nisi suscipit cumque!
+        </div>
+        <a class="btn1" href="">About me</a>
     </div>
-    <div class="tittle2">
-        <h5 class="text-tittle2">
-            Stylist & fashion editor
-        </h5>
-    </div>
-    <img class="img-fluid image-show" src="./images/wenyang-CY_c0cR01AQ-unsplash-768x1024.png" alt="" class="img-responsive">
-    <div class="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae voluptatem sed dignissimos sit cumque iusto! Corporis, labore? Id sequi magnam vel cum error adipisci nisi suscipit cumque!
-    </div>
-    <a class="btn1" href="">About me</a>
-</div>
 </div>

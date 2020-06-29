ba6c18af75f4c4c1bc6e8cd3ab6313246aeb0ddd
Nguyễn Chí Tân <44807008+TANNGUYEN1999@users.noreply.github.com>
2020-06-29 18:22:08 +0700

    update source test

---
+++ 24/24-content.php
@@ -0,0 +1,90 @@
+<div class="type-24">
+    <div class="container item">
+        <div class="row">
+            <div class="col-md-4 a1">
+                <div class="text-tittle1">
+                    <h2 class="tittle5">Central Office</h2>
+                </div>
+
+                <div class="text-meta">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea in quam autem quos quisquam eum. In, saepe eos consectetur nostrum repellendus eaque harum molestiae accusamus, voluptatum vero, mollitia ipsa maiores.
+                </div>
+                <div class="list">
+                    <div class="list1">
+                        <i class="fas fa-phone-volume icon2"></i>
+                        <span class="info">0(800) 890-90-609</span>
+                    </div>
+                    <div class="list1">
+                        <i class="fab fa-skype icon2"></i>
+                        <span class="info">SkyName</span>
+                    </div>
+                    <div class="list1">
+                        <i class="fas fa-map-marker-alt icon2"></i>
+                        <span class="info">29 Nicolas str.New York, 987597-50</span>
+                    </div>
+
+                </div>
+
+                <i class="fab fa-facebook-f icon1"></i>
+                <i class="fab fa-twitter icon1"></i>
+                <i class="fab fa-youtube-square icon1"></i>
+                <i class="fab fa-instagram icon1"></i>
+
+
+
+            </div>
+            <div class="col-md-4 a2">
+                <div class="text-tittle2">
+                    <h2 class="tittle6">Opening Hours</h2>
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Monday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Tuesday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Wednesday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Thursday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Friday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Saturday</span>
+                    9:00 - 19:00
+                </div>
+                <div class="opening-hourse">
+                    <span class="textday">Sunday</span>
+                    9:00 - 19:00
+                </div>
+            </div>
+            <div class="col-md-4 a3">
+                <form action="#" method="GET" class="text-form">
+                    <div class="text-tittle">
+                        Your Name
+                    </div>
+                    <input type="text" name="name" class="text-input">
+                    <div class="text-tittle">
+                        Your Email
+                    </div>
+                    <input type="text" name="email" class="text-input">
+                    <div class="text-tittle">
+                        Your Message
+                    </div>
+                    <textarea name="message" cols="23" rows="5" class="text-input"></textarea><br>
+                    <div class="submit">
+                        <button name="submit" type="button" class="btn">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
